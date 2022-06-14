842132b471194a17e53ddd81d3fa808542e09b64
Ricardo Montoya <loquendo3277@gmail.com>
2022-06-14 14:42:16 -0500

    Style: Adding new CSS options and input tags

--- src/App.js
+++ src/App.js
@@ -3,50 +3,55 @@ import "./App.css";
 
 function App() {
 
-  const [people, setPeople] = useState([
-    {
-      name: "John",
-      lastName: "Doe",
-      age: 30,
-      favouriteFood: "Pizza",
-      favouriteColour: "Red",
-      counter: 0,
-    },
-  ]);
+    const [people, setPeople] = useState([{
+        name: "John",
+        lastName: "Doe",
+        age: 30,
+        favouriteFood: "Pizza",
+        favouriteColour: "Red",
+        counter: 0,
+    }, ]);
 
-  const [name, setName] = useState("");
-  const [lastName, setLastname] = useState("");
+    const [name, setName] = useState("");
+    const [lastName, setLastname] = useState("");
 
-  return (
-    <div className="App">
-      <input
-        type="text"
-        name="name"
-        onChange={(event) => {
-          setName(event.target.value);
-        }}
-      />
-      <input
-        type="text"
-        name="lastName"
-        onChange={(event) => {
-          setLastname(event.target.value);
-        }}
-      />
-      <button
-        onClick={() => {
-          setPeople((current) => [{ name, lastName }, ...current]);
-        }}
-      >
-        Add user
-      </button>
-      <ul>
-        {people.map((person, idx) => (
-          <li id={idx}>{`${person.name} ${person.lastName}`}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        input type = "text"
+        name = "name"
+        className = "Name"
+        onChange = {
+            (event) => {
+                setName(event.target.value);
+            }
+        }
+        /> <
+        input type = "text"
+        name = "lastName"
+        className = "Lastname"
+        onChange = {
+            (event) => {
+                setLastname(event.target.value);
+            }
+        }
+        /> <
+        button className = "btn"
+        onClick = {
+            () => {
+                setPeople((current) => [{ name, lastName }, ...current]);
+            }
+        } >
+        Add user <
+        /button> <
+        ul > {
+            people.map((person, idx) => ( <
+                li id = { idx } > { `${person.name} ${person.lastName}` } < /li>
+            ))
+        } <
+        /ul> <
+        /div>
+    );
 }
 
 export default App;
